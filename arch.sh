b5539971d5acb4406c72d8dee85e855cf0135e35
Lintech <81373196+Lintech-1@users.noreply.github.com>
2022-04-27 19:52:05 +0300

    Add files via upload

--- arch.sh
+++ arch.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# LOG version 1.5.0
+# LOG version 1.5.1
 # author: Keper#6769
 # YouTube: https://www.youtube.com/c/Lintech8
 # Codeberg: https://codeberg.org/Lintech
 # github: https://github.com/Lintech-1/arch-script/
 #--------------------------------------------------------------------------------------
-# - Изменём задержки запуска команда с 5 до 3-х секунд 
-# - Добавлен выбор установки между yay и pikaur
-# - Добавлен выбор установка между редактором nano и vim
-# - Теперь строчка miltulib в pacman.conf будет автоматически раскомментирован
+# - Добавлен пакет xdg-desktop-portal для pipewire
+# - Добавлена fix для pipewire в виде команды 
+# - Добавлены пакеты libpulse lib32-libpulse для pipewire
+# - 
 #--------------------------------------------------------------------------------------
-echo "Version 1.5.0
+echo "Version 1.5.1
 ░██████╗███████╗████████╗██╗░░░██╗██████╗░
 ██╔════╝██╔════╝╚══██╔══╝██║░░░██║██╔══██╗
 ╚█████╗░█████╗░░░░░██║░░░██║░░░██║██████╔╝
@@ -56,7 +56,8 @@ echo "вы хотите установить pipewire?
 echo -n "введите цифру: "
 read VAR
 if [[ "$VAR" == 1 ]]; then
-  sudo pacman -S pipewire pipewire-alsa pipewire-jack lib32-pipewire lib32-pipewire-jack
+  sudo pacman -S pipewire pipewire-alsa pipewire-jack lib32-pipewire lib32-pipewire-jack libpulse lib32-libpulse xdg-desktop-portal
+  systemctl --user enable pipewire
 elif [[ "$VAR" == 2 ]]; then
   sudo pacman -S pulseaudio pulseaudio-alsa alsa-utils
   pulseaudio -D
